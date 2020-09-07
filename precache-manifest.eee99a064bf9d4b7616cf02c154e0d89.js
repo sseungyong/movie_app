76648fb0360d5e1c6ea85d55805a3421176c4019
SEUNGYONG KIM <sseungyong@gmail.com>
2020-09-08 01:02:58 +0900

    Updates

---
+++ precache-manifest.eee99a064bf9d4b7616cf02c154e0d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96694861cca9e2db02e1942fa77e0f74",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0c30b839da35c0901e92",
+    "url": "/movie_app/static/css/main.5f423b65.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c30b839da35c0901e92",
+    "url": "/movie_app/static/js/main.fda270bf.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
